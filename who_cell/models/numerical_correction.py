8586cb61733517c19a280a83acb422b70e7989a1
bp6725 <30527115+bp6725@users.noreply.github.com>
2021-10-30 12:54:39 +0300

    small change

--- who_cell/models/numerical_correction.py
+++ who_cell/models/numerical_correction.py
@@ -16,7 +16,7 @@ class NumericalCorrection():
 
         results = []
         gs = GibbsSampler(N,multi_process=self.multi_process)
-        for pc in np.linspace(0.1,1,10) :
+        for pc in np.linspace(0.1,1,5) :
             gussed_reconstructed_transitions = NumericalCorrection.reconstruct_full_transitions_dict_from_few(naive_transitions_matrix,
                                                                                                               pc,start_probs)
             _,_, seq_probs,_,_,_= gs.sample_known_transitions( all_relvent_observations,
