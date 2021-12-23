b37666188191d711314a39d0d0dc4e1f6d2c970a
bp67250 <bp6725007@gmail.com>
2021-12-23 23:22:35 +0200

    Auto stash before merge of "master" and "origin/master"

--- who_cell/simulation/known_transition_matrices.py
+++ who_cell/simulation/known_transition_matrices.py
@@ -45,11 +45,11 @@ class KnownTransition_matrices() :
 
     @staticmethod
     def get_POS_data():
-        with open("../../who_cell/simulation/pos_transitions_probs", 'rb') as f:
+        with open("../../who_cell/simulation/pos_transitions_probs",'rb') as f :
             transition_dict = pickle.load(f)
-        with open("../../who_cell/simulation/start_probs", 'rb') as f:
+        with open("../../who_cell/simulation/start_probs",'rb') as f :
             start_probs = pickle.load(f)
-        with open("../../who_cell/simulation/emissions", 'rb') as f:
+        with open("../../who_cell/simulation/emissions",'rb') as f :
             emmisions = pickle.load(f)
 
         transition_dict["start"] = start_probs
