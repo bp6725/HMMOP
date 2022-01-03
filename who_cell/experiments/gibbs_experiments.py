5182a8eb28e69b0b601a64b255655652c7abafe4
bp6725 <30527115+bp6725@users.noreply.github.com>
2022-01-03 21:16:19 +0200

    performance

--- who_cell/experiments/gibbs_experiments.py
+++ who_cell/experiments/gibbs_experiments.py
@@ -296,11 +296,11 @@ class GibbsExperiment() :
                                                    Ng_iters=params['N_itres'],
                                                    w_smapler_n_iter=w_smapler_n_iter, is_mh=params["is_mh"])
 
-                sampled_transitions_dict = all_sampled_transitions[-1]
-                sampled_mues = None
-                all_states = None
-                all_observations_sum = None
-                all_mues = None
+            sampled_transitions_dict = all_sampled_transitions[-1]
+            sampled_mues = None
+            all_states = None
+            all_observations_sum = None
+            all_mues = None
 
         if relevent_sampling_method == "PC from outside":
             if not is_known_emmi :
