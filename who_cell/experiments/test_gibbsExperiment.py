940769f7c30ffedd334dbad57488022764a51218
bp6725 <30527115+bp6725@users.noreply.github.com>
2022-01-04 12:17:49 +0200

    performance

--- who_cell/experiments/test_gibbsExperiment.py
+++ who_cell/experiments/test_gibbsExperiment.py
@@ -20,7 +20,7 @@ class TestGibbsExperiment(TestCase):
             "bipartite": [False],
             "inner_outer_trans_probs_ratio": [300],
             'n_states': [10],
-            "known_dataset":["SCHIZX1_drug"],
+            #"known_dataset":["SCHIZX1_drug"],
             'sigma': [0.1],
             'number_of_smapled_traj': [1000],
             # 'p_prob_of_observation': [0.5, (0.5, 0.1), (0.55, 0.45, 0.45, 0.55)],
@@ -41,30 +41,29 @@ class TestGibbsExperiment(TestCase):
         params_dict = {
             'is_acyclic': [True],
             'known_mues': [True],
-            #     "is_few_observation_model":[True,False],
-            "is_few_observation_model": [False],
-            "is_only_seen": ['observed', "all"],
-            'N': [50],
+            "is_few_observation_model": [True, False],
+            "is_only_seen": ["all", "observed"],
+            'N': [20],
             'd': [5],
             "bipartite": [False],
             "inner_outer_trans_probs_ratio": [50],
-            'n_states': [10],
-            # "known_dataset": ["SCHIZX1_plcebo", "SCHIZX1_drug"],
+            'n_states': [1],
+            "known_dataset": ["SCHIZX1_plcebo", "SCHIZX1_drug", "POS"],
             'sigma': [0.1],
-            'number_of_smapled_traj': [1000],
-            #     'p_prob_of_observation': [0.5],
-            'p_prob_of_observation': [1],
-            'N_itres': [100],
+            'number_of_smapled_traj': [15],
+            'p_prob_of_observation': [(0.5, 0.05), (0.5, 0.1), (0.5, 0.2), (0.5, 0.3)],
+            'N_itres': [1],
             'is_mh': [False],
-            'w_smapler_n_iter': [120],
-            #     'is_known_W':[True,False],
-            'is_known_W': [False],
+            'w_smapler_n_iter': [100],
+            'is_known_W': [True],
             "is_multi_process": [True],
-            "PC_guess": [0.25, -1],
-            "N_guess": [-1, 4]
+            "PC_guess": [0.5],
+            "numerical_reconstruction_pc": [0.5],
+            "is_numerical_reconstruction_method": [True, False]
         }
-
-        GibbsExperiment.run_multi_params_and_plot_report(params_dict,model_defining_params_pre,skip_sampler = False)
+        all_models_results_known_sets = GibbsExperiment.run_multi_params_and_return_results(params_dict,
+                                                                                            model_defining_params_pre,
+                                                                                            skip_sampler=False)
 
     def test_run_multi_params_and_return_results(self):
         model_defining_params_pre = ['N', "d", "n_states"]
